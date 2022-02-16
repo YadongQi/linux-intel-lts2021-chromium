f089ebd02d31e1cf07e50d5a8ae89ff908a6d89f
Jason Chen CJ <jason.cj.chen@intel.com>
2022-02-16 09:46:43 +0800

    pkvm: x86: introduce CONFIG_PKVM_INTEL
    This patch add an PKVM_INTEL configuration to enable or disable pKVM
    feature on X86 platform (starting from Intel platform), and do
    pkvm_init as an extension of kvm.
    
    dir arch/x86/kvm/vmx/pkvm is created to hold pkvm x86 files.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>
    Reviewed-by: Eddie Dong <eddie.dong@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/include/pkvm.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#ifndef _PKVM_H_
+#define _PKVM_H_
+
+struct pkvm_hyp {
+	int num_cpus;
+};
+
+#define PKVM_PAGES (ALIGN(sizeof(struct pkvm_hyp), PAGE_SIZE) >> PAGE_SHIFT)
+
+#endif
