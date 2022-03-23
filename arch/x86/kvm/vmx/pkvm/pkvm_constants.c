da62ff1b7a6fc938b29e69d12756cf66eb29cffb
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-23 21:27:23 +0800

    pkvm: x86: generate pkvm_constants.h for the pkvm initialization
    In order to avoid exposing pkvm hypervisor runtime data structures directly
    to the pkvm init-time, generate pkvm_constants.h to provide constants such
    as structure size to the pkvm init without dragging in the definitions
    themselves.
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/pkvm_constants.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+
+#include <linux/kbuild.h>
+#include <pkvm.h>
+
+int main(void)
+{
+	DEFINE(PKVM_PERCPU_PAGES, PKVM_PCPU_PAGES + PKVM_HOST_VCPU_PAGES);
+	DEFINE(PKVM_GLOBAL_PAGES, PKVM_PAGES);
+	return 0;
+}
