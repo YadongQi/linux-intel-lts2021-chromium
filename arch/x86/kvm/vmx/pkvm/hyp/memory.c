e7ee61d9da1f5087217c6e3f5a83ddec2b161e72
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-24 18:43:34 +0800

    pkvm: x86: define hypervisor runtime VA/PA APIs
    This patch setups the translation between VA & PA for pkvm hypervisor
    runtime:
    keep same direct mapping as Linux kernel based on __page_base_offset.
    also keep same pkvm symbol mapping as Linux kernel based on
    __symbol_base_offset.
    
    direct mapping translation:
    - pkvm_phys_to_virt: phys + __page_base_offset
    - pkvm_virt_to_phys: virt - __page_base_offset
    symbol mapping translation:
    - pkvm_virt_to_symbol_phys: virt - __symbol_base_offset
    
    Signed-off-by: Shaoqin Huang <shaoqin.huang@intel.com>
    Signed-off-by: Chuanxiao Dong <chuanxiao.dong@intel.com>
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/memory.c
@@ -0,0 +1,23 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <linux/types.h>
+
+unsigned long __page_base_offset;
+unsigned long __symbol_base_offset;
+
+void *pkvm_phys_to_virt(unsigned long phys)
+{
+	return (void *)__page_base_offset + phys;
+}
+
+unsigned long pkvm_virt_to_phys(void *virt)
+{
+	return (unsigned long)virt - __page_base_offset;
+}
+
+unsigned long pkvm_virt_to_symbol_phys(void *virt)
+{
+	return (unsigned long)virt - __symbol_base_offset;
+}
