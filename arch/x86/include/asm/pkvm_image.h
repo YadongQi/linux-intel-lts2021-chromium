d9595ac95033e1ae6470979c5d1508f76b890a6b
Jason Chen CJ <jason.cj.chen@intel.com>
2022-03-25 20:46:01 +0800

    pkvm: x86: define linker script alias for kernel-proper symbol
    defines a linker script alias of a kernel-proper symbol referenced by
    PKVM code. user can add alias symbol in asm/pkvm_image_vars.h, this
    patch also added some alias symbols for later mmu/ept pgtable usage.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

--- arch/x86/include/asm/pkvm_image.h
+++ arch/x86/include/asm/pkvm_image.h
@@ -35,6 +35,12 @@
 		*(NAME NAME##.*)		\
 	END_PKVM_SECTION
 
+/*
+ * Defines a linker script alias of a kernel-proper symbol referenced by
+ * PKVM code.
+ */
+#define PKVM_ALIAS(sym)  pkvm_sym(sym) = sym;
+
 #endif /* LINKER_SCRIPT */
 
 #endif /* __X86_INTEL_PKVM_IMAGE_H */
---
+++ arch/x86/include/asm/pkvm_image_vars.h
@@ -0,0 +1,11 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+#ifndef __ASM_x86_PKVM_IMAGE_VARS_H
+#define __ASM_x86_PKVM_IMAGE_VARS_H
+
+PKVM_ALIAS(physical_mask);
+PKVM_ALIAS(sme_me_mask);
+PKVM_ALIAS(__default_kernel_pte_mask);
+
+#endif
