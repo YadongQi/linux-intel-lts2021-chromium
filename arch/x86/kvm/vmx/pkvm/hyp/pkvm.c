5786bafb75364c6f70b4de1f19fbeb385dd77f53
Jason Chen CJ <jason.cj.chen@intel.com>
2022-07-01 21:58:39 +0800

    pkvm: x86: add global pkvm_hyp pointer
    A global pointer pkvm_hyp is added to pkvm hypervisor and it will be
    used in pkvm hypervisor runtimer.
    
    Signed-off-by: Jason Chen CJ <jason.cj.chen@intel.com>

---
+++ arch/x86/kvm/vmx/pkvm/hyp/pkvm.c
@@ -0,0 +1,7 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+
+#include <pkvm.h>
+
+struct pkvm_hyp *pkvm_hyp;
---
+++ arch/x86/kvm/vmx/pkvm/hyp/pkvm_hyp.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: GPL-2.0
+ */
+#ifndef __PKVM_HYP_H
+#define __PKVM_HYP_H
+
+extern struct pkvm_hyp *pkvm_hyp;
+
+#endif
